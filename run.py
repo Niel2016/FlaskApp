2ca7809193f33c13331b40ead3d38262ae067018
Niel <montainlion2012.cn@gmail.com>
2016-08-18 17:53:29 +0800

    add manager.run

--- run.py
+++ run.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g, abort
+from flask_script import Manager
 
 
 app = Flask(__name__)
+manager = Manager(app)
+
+app.config['DEBUG'] = True
+@app.before_request
+def before_first_request():
+  g.user = 'dave'
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+  # abort(404)
+  return render_template('index.html',
+                         name=g.user)
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  manager.run()
