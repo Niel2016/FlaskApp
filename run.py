47acedddd055f6b70f4952291d83125d8f49e3ca
niel <montainlion2012.cn@gmail.com>
2016-08-19 17:41:11 +0800

    add bootstrap template into index.html

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, g, abort, request
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 manager = Manager(app)
 
 app.config['DEBUG'] = True
 @app.before_request
 def before_first_request():
-  g.user = 'dave'
+  g.user = 'Niel'
 
 @app.route('/')
 def home():
