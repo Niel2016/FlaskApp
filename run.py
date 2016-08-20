dc2f1fe829a034a05bd6a9536d7e13fa563dbd2f
kelvin2015 <calvin@imac.local>
2016-08-20 09:49:24 +0800

    add 505.html

--- run.py
+++ run.py
@@ -1,28 +1,34 @@
-from flask import Flask, render_template, g, abort, request
+from flask import Flask
+from flask import render_template
+from flask import g
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 manager = Manager(app)
 
 app.config['DEBUG'] = True
+
+
 @app.before_request
 def before_first_request():
-  g.user = 'Niel'
+    g.user = 'Niel'
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html',
-							msg=e)
+    return render_template('404.html', msg=e)
+
+
+@app.errorhandler(505)
+def server_internal_error(e):
+    return render_template('505.html', s_msg=e)
+
 
 @app.route('/')
 def home():
-  # abort(404)
-  return render_template('index.html',
-                         name=g.user)
+    return render_template('index.html', name=g.user)
 
 if __name__ == '__main__':
-  'This is main function.'
-  manager.run()
+    manager.run()
