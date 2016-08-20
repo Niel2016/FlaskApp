88c1b780c99395809f206a04f7c6ae9bf354dbc6
kelvin2015 <calvin@imac.local>
2016-08-20 11:36:32 +0800

    add flask-momnent extension

--- run.py
+++ run.py
@@ -1,19 +1,21 @@
+from datetime import datetime
 from flask import Flask
 from flask import render_template
-from flask import g
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 manager = Manager(app)
+moment = Moment(app)
 
 app.config['DEBUG'] = True
 
 
-@app.before_request
-def before_first_request():
-    g.user = 'Niel'
+# @app.before_request
+# def before_first_request():
+#     g.user = 'Nielss'
 
 
 @app.errorhandler(404)
@@ -21,14 +23,15 @@ def page_not_found(e):
     return render_template('404.html', msg=e)
 
 
-@app.errorhandler(505)
+@app.errorhandler(500)
 def server_internal_error(e):
-    return render_template('505.html', s_msg=e)
+    return render_template('500.html', s_msg=e)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', name=g.user)
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 if __name__ == '__main__':
     manager.run()
