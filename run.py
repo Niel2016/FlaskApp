bdaca896bcf35e6ce017b32d27e4a29726369929
kelvin2015 <calvin@imac.local>
2016-08-20 16:41:11 +0800

    add web form function.

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 from datetime import datetime
-from flask import Flask
+from flask import Flask, flash
+from flask import session, redirect, url_for
 from flask import render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -11,11 +16,15 @@ manager = Manager(app)
 moment = Moment(app)
 
 app.config['DEBUG'] = True
-
-
+app.config['SECRET_KEY'] = 'HARD TO GUESS'
 # @app.before_request
 # def before_first_request():
 #     g.user = 'Nielss'
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -28,9 +37,19 @@ def server_internal_error(e):
     return render_template('500.html', s_msg=e)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    form = NameForm()
+    if form.validate_on_submit():
+        oldname = session.get('name')
+        if oldname is not None and oldname != form.name.data:
+            flash('Looks like you have changed your name.')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('home'))
     return render_template('index.html',
+                           username=session.get('name'),
+                           form=form,
                            current_time=datetime.utcnow())
 
 if __name__ == '__main__':
