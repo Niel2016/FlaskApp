7e0cac25fcb75c75987ea0fbb45b9cfb176bed12
Niel <montainlion2012.cn@gmail.com>
2016-08-23 10:27:02 +0800

    add registration.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,16 +1,39 @@
-from flask import render_template, redirect, url_for, session, flash
+from flask import render_template, redirect, url_for, session, flash, request
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from ..models import User, Role
+from flask_login import login_user, logout_user
+from .. import db
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
   form = LoginForm()
   if form.validate_on_submit():
     user = User.query.filter_by(email=form.email.data).first()
-    if user is not None and user.verify_password(form.email.data):
+    if user is not None and user.verify_password(form.password.data):
       login_user(user, form.remember_me.data)
-      return redirect(url_for('main.index'))
+      return redirect(request.args.get('next') or url_for('main.home'))
     flash('Invalid email or password.')
-  return render_template('auth/login.html',
+  return render_template('auth/login.html', form=form)
+
+@auth.route('/logout')
+def logout():
+  logout_user()
+  flash('You have been logged out.')
+  return redirect(url_for('main.home'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(username=form.username.data,
+                email=form.email.data,
+                password=form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash('You can now login.')
+    return redirect(url_for('auth.login'))
+  return render_template('auth/register.html',
                          form=form)
+
--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
+
   __tablename__ = 'users'
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(64), unique=True, index=True)
@@ -15,7 +17,7 @@ class User(db.Model):
 
   @password.setter
   def password(self, password):
-    self.password_hash = generate_password_hash()
+    self.password_hash = generate_password_hash(password)
 
   def verify_password(self, password):
     return check_password_hash(self.password_hash, password)
@@ -25,6 +27,7 @@ class User(db.Model):
 
 
 class Role(db.Model):
+
   __tablename__ = 'roles'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(64), unique=True, index=True)
